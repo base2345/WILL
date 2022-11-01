f64ee305329e91e2699c435471a7a8a16b2d6edf
base2345 <wsgwilliam2557@gmail.com>
2022-11-01 12:21:05 -0500

    canival corpse

--- 01.js
+++ 01.js
@@ -4,15 +4,28 @@
   código dentro de las funciones ya definidas. 
   No comentar la funcion 
 */
-function soloNumeros(array) {
-  // La funcion llamada 'soloNumeros' recibe como argumento un arreglo de enteros y strings llamado 'array'.
+ // La funcion llamada 'soloNumeros' recibe como argumento un arreglo de enteros y strings llamado 'array'.
   // Debe devolver un arreglo con solo los enteros.
   // Ej: 
   // soloNumeros([1, 'Henry', 2]) debe retornar [1, 2]
 
   // Tu código aca:
-
+function soloNumeros(array) 
+{
+  var incrementa = []
+  for(i = 0; i < array.length; i++)
+  {
+    if(array[i]>=0   || array[i]===10)
+    {
+    incrementa.push(array[i])
+    }
+  }
+  return incrementa
 }
+//console.log(soloNumeros([2,"wil",4,"sa",5,6,7]))
+
+
+
 
 // No modifiques nada debajo de esta linea //
 
